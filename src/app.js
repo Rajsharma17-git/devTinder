0cd1691e0bf85a45b231ae452b342a8dfd6869a4
Rajsharma17-git <rajsharma17112005@gmail.com>
2025-02-10 16:38:03 +0530

    exploring get, post , delete

--- src/app.js
+++ src/app.js
@@ -3,11 +3,19 @@ const express= require('express');
 
 const app = express();
 
-app.use((req, res) => {
-   res.send("Hello from the server!");
-});
+//app.use("/" ,(req, res) => {
+  // res.send("Hello from the server!");
+//});
+
+app.get("/user",(req, res) => {
+    res.send({ firstname:"Raj" });
+ });
+
+ app.use("/test",(req, res) => {
+    res.send("Hello Raj!");
+ });
 
 
-app.listen(3005, () => {
-    console.log("server is successfully listening on port 3005...");
+app.listen(7777, () => {
+    console.log("server is successfully listening on port 7777...");
 });
