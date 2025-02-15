bee8ec6a4de13dd8d296fca84af9554135308582
Rajsharma17-git <rajsharma17112005@gmail.com>
2025-02-15 11:55:21 +0530

    get apis

--- src/app.js
+++ src/app.js
@@ -17,6 +17,30 @@ try {
 }  
 });
 
+// get user by email
+app.get("/user", async(req, res) => {
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({ emailId: userEmail });
+        if(users.length === 0) {
+            res.status(404).send("user not found");
+        }else {
+            res.send(users);
+        }
+    } catch(err) {
+        res.status(400).send("something went wrong");
+    }
+});
+
+//feed
+app.get("/feed", async(req, res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }catch(err) {
+        res.status(400).send("something went wrong");
+    }
+});
 
 connectDB()
 .then(() => {
